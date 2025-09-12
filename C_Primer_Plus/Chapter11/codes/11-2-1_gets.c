cbc2b7c878b1216392ee483669b73a33bfe9f308
GHzQcE <guojinghao2020@outlook.com>
2025-09-12 22:24:45 +0800

    #

--- C_Primer_Plus/Chapter11/codes/11-2-1_gets.c
+++ C_Primer_Plus/Chapter11/codes/11-2-1_gets.c
@@ -6,10 +6,11 @@ int main(void)
     char words[50];
     puts("please enter some words.");
     //gets(words);    //如果你用vscode这里已经报警告了，我这里把它注释掉
-    puts(words);
+    //puts(words);
     printf("%s", words);
     puts("done.");   // 这里可以看到，gets()会在识别到换行符...
                      //...时终止输入，并去掉换行符。而puts()会在输出是加上一个换行符
+    return 0;
 }
 //由于puts()只有 提前声明的一个字符串变量 一个参数，并没有最大可接受的字符串长度...
 //因此当输入的字符串长度过长时会导致缓冲区溢出，从而“导致一系列安全风险”
---
+++ C_Primer_Plus/Chapter11/codes/11-2-2_fgets.c
@@ -0,0 +1,13 @@
+//fgets()，不小心把这个文件删了QuQ
+#include <stdio.h>
+int main(void)
+{
+    char words[50];
+    char * ptr;
+
+    ptr = fgets(words, 50, stdin);  //参数为，赋给变量，最大长度，要读入的文件，返回字符串的指针或NULL
+    fputs(words, stdout);   //同上
+    fputs(ptr, stdout);
+
+    return 0;
+}
