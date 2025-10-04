78440c83757dff76e61d49c54349a25c3f5340ac
GHzQcE <guojinghao2020@outlook.com>
2025-10-04 09:23:59 +0800

    #

--- C_Primer_Plus/Chapter11/codes/11-3-6_strncat.c
+++ C_Primer_Plus/Chapter11/codes/11-3-6_strncat.c
@@ -9,7 +9,9 @@ int main(void)
     gets_s(words1, 20);
     gets_s(words2, 20);
     
-    strncat(words1, words2, 3);
+    strncat(words1, words2, 3);     //前面的保留完整，后面的取n个字符接在前面
 
-    puts    //好困，，，明天再学
+    puts(words1);
+
+    return 0;
 }
